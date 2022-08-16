ca2b60f79699be0919ef64e6664dc233184ee74f
Ujjwal <ugautam@hawaii.edu>
2022-08-16 11:10:10 -1000

    added more regEx

--- regex.js
+++ regex.js
@@ -16,10 +16,33 @@ const result2 = test.match(regEx); //returns array of matched items if global fl
 let text = "<h1>Winter is coming</h1>";
 let myRegex = /<[a-z]1>+?/; // Change this line, question mark is lazy loading matches the smallest pattern
 let result = text.match(myRegex);
-console.log(result);
+// console.log(result);
 
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /[\w+]/g; // Change this line
 let result3 = quoteSample.match(alphabetRegexV2);
-
-console.log(result3);
+let quoteSample2 = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /[^\w+]/g; // Change this line
+let result4 = quoteSample2.match(nonAlphabetRegex);
+let movieName = "2001: A Space Odyssey";
+let numRegex = /[\d+]/g; // Change this line
+let result5 = movieName.match(numRegex);
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /[\s+]/g; // Change this line
+let result6 = sample.match(countWhiteSpace);
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i; // Change this line
+let result7 = userCheck.test(username);
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6} no/g; // Change this line, matches between 3-6 h's
+let result8 = ohRegex.test(ohStr);
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/; // Change this line, matches with more than 4 z's
+let result9 = haRegex.test(haStr);
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/; // Change this line, matches exactly 4 m's
+let result10 = timRegex.test(timStr);
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line, matches either favorite or favourite
+let result11 = favRegex.test(favWord);
+console.log(result11);
