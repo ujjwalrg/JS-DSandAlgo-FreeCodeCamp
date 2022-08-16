52fb55244caf7b18ad92ba8ab042ed62a5786df9
Ujjwal <ugautam@hawaii.edu>
2022-08-16 09:28:18 -1000

    regex tutorial

--- regex.js
+++ regex.js
@@ -11,4 +11,9 @@ const result2 = test.match(regEx); //returns array of matched items if global fl
 // } else {
 //   console.log("nothing");
 // }
-result2 ? console.log("something") : console.log("nothing");
+// result2 ? console.log("something") : console.log("nothing");
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<[a-z]1>+?/; // Change this line, question mark is lazy loading matches the smallest pattern
+let result = text.match(myRegex);
+console.log(result);
