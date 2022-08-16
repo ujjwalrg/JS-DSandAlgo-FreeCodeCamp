daf5cdcba2f76e79352ff5c07eebe38dd7b76b2b
Ujjwal <ugautam@hawaii.edu>
2022-08-16 09:14:48 -1000

    regex tutorial

--- regex.js
+++ regex.js
@@ -1,8 +1,14 @@
 // true if the string contains a number
-const test = "I am a hero i am"; //false
-const test2 = "I am a hero 0i am"; //true
+const test = "I am a hero 012i am@"; //false
+const test2 = "I am a hero i am"; //true
 const regEx = /[^a-z ]/gi;
 // const result = test.match(regEx);
-const result = regEx.test(test);
+// const result = regEx.test(test); //returns true/false
+const result2 = test.match(regEx); //returns array of matched items if global flag is set, otherwise returns one item
 
-console.log(result);
+// if (result2) {
+//   console.log("something");
+// } else {
+//   console.log("nothing");
+// }
+result2 ? console.log("something") : console.log("nothing");
