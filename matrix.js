f90faba563f56fb0c348c6521f07e73a1f200295
Ujjwal <ugautam@hawaii.edu>
2022-08-18 10:50:43 -1000

    some js  practice

--- matrix.js
+++ matrix.js
@@ -1,14 +1,41 @@
-// let foods = {
-  apples: 25,
-  oranges: 32,
-  plums: 28
+// // let foods = {
+//   apples: 25,
+//   oranges: 32,
+//   plums: 28
+// };
+
+// // Only change code below this line
+
+// foods.bananas = 13;
+// foods["grapes"] = 35;
+// foods.strawberries = 27;
+// // Only change code above this line
+
+// console.log(foods);
+const users = {
+  Alan: {
+    online: false,
+  },
+  Jeff: {
+    online: true,
+  },
+  Sarah: {
+    online: false,
+  },
 };
 
-// Only change code below this line
+function countOnline(usersObj) {
+  // Only change code below this line
+  let count = 0;
+  for (let user in usersObj) {
+    if (usersObj[user].online) {
+      console.log(typeof user);
+      count = count + 1;
+    }
+  }
+  return count;
 
-foods.bananas = 13;
-foods["grapes"] = 35;
-foods.strawberries = 27;
-// Only change code above this line
+  // Only change code above this line
+}
 
-console.log(foods);
+console.log(countOnline(users));
