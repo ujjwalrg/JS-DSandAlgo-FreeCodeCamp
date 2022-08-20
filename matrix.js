bcd5365d8b33f2ebbaae41206cd4144f2f834478
Ujjwal <ugautam@hawaii.edu>
2022-08-19 20:16:19 -1000

    some js  practice

--- matrix.js
+++ matrix.js
@@ -1,67 +1,67 @@
-// // // let foods = {
-// //   apples: 25,
-// //   oranges: 32,
-// //   plums: 28
-// // };
+// // // // let foods = {
+// // //   apples: 25,
+// // //   oranges: 32,
+// // //   plums: 28
+// // // };
 
-// // // Only change code below this line
+// // // // Only change code below this line
 
-// // foods.bananas = 13;
-// // foods["grapes"] = 35;
-// // foods.strawberries = 27;
-// // // Only change code above this line
+// // // foods.bananas = 13;
+// // // foods["grapes"] = 35;
+// // // foods.strawberries = 27;
+// // // // Only change code above this line
 
-// // console.log(foods);
-// const users = {
-//   Alan: {
-//     online: false
+// // // console.log(foods);
+// // const users = {
+// //   Alan: {
+// //     online: false
+// //   },
+// //   Jeff: {
+// //     online: true
+// //   },
+// //   Sarah: {
+// //     online: false
+// //   }
+// // }
+
+// // function countOnline(usersObj) {
+// //   // Only change code below this line
+// //   let count = 0;
+// //   for (let user in usersObj){
+// //     if (usersObj[user].online){
+// //       console.log(typeof user);
+// //       count = count + 1;
+// //     }
+// //   }
+// //   return count;
+
+// //   // Only change code above this line
+// // }
+
+// // console.log(countOnline(users));
+// let user = {
+//   name: "Kenneth",
+//   age: 28,
+//   data: {
+//     username: "kennethCodesAllDay",
+//     joinDate: "March 26, 2016",
+//     organization: "freeCodeCamp",
+//     friends: ["Sam", "Kira", "Tomo"],
+//     location: {
+//       city: "San Francisco",
+//       state: "CA",
+//       country: "USA",
+//     },
 //   },
-//   Jeff: {
-//     online: true
-//   },
-//   Sarah: {
-//     online: false
-//   }
-// }
+// };
 
-// function countOnline(usersObj) {
+// function addFriend(userObj, friend) {
 //   // Only change code below this line
-//   let count = 0;
-//   for (let user in usersObj){
-//     if (usersObj[user].online){
-//       console.log(typeof user);
-//       count = count + 1;
-//     }
-//   }
-//   return count;
-
+//   let myArray = [];
+//   userObj.data.friends.push(friend);
+//   myArray = myArray.concat(userObj.data.friends);
+//   return myArray;
 //   // Only change code above this line
 // }
 
-// console.log(countOnline(users));
-let user = {
-  name: "Kenneth",
-  age: 28,
-  data: {
-    username: "kennethCodesAllDay",
-    joinDate: "March 26, 2016",
-    organization: "freeCodeCamp",
-    friends: ["Sam", "Kira", "Tomo"],
-    location: {
-      city: "San Francisco",
-      state: "CA",
-      country: "USA",
-    },
-  },
-};
-
-function addFriend(userObj, friend) {
-  // Only change code below this line
-  let myArray = [];
-  userObj.data.friends.push(friend);
-  myArray = myArray.concat(userObj.data.friends);
-  return myArray;
-  // Only change code above this line
-}
-
-console.log(addFriend(user, "Pete"));
+// console.log(addFriend(user, "Pete"));
