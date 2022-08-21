d98322ab654d570419a2536cef784c6172c3a3b3
Ujjwal <ugautam@hawaii.edu>
2022-08-20 17:16:34 -1000

    some js  practice

--- matrix.js
+++ matrix.js
@@ -172,3 +172,10 @@
 // }
 
 // booWho(null);
+function frankenSplice(arr1, arr2, n) {
+  arr2 = arr2.slice(0, n).concat(arr1).concat(arr2.slice(n, arr2.length));
+  console.log(arr2);
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
